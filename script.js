2ffcc6e08f9c254a1a9520a78b7c9bada31a3625
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-04-17 15:06:13 -0300

    feat: add scrollIntoView, private messages and online users selection

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let userName;
 let userNameInput;
+let userSelected;
+let selectedContact;
+let selectedUser = { name: "Todos" };
 function userRequest(name) {
   userName = {
     name,
@@ -29,17 +32,31 @@ function getMessage() {
     axios
       .get("https://mock-api.driven.com.br/api/v6/uol/messages")
       .then(processResponse);
+    scroll();
   }, 3000);
+}
+
+function getUsers() {
+  setInterval(function () {
+    axios
+      .get("https://mock-api.driven.com.br/api/v6/uol/participants")
+      .then(usersResponse);
+  }, 10000);
 }
 
 function sendMessage() {
   const from = userNameInput;
-  const to = "Todos";
+  const to = document.querySelector(".selectedContact > div > p").innerHTML;
   const text = document.querySelector("#sendText").value;
-  const type = "message";
+  const type =
+    document.querySelector(".selectedVisibility > div > p").innerHTML ===
+    "Público"
+      ? "message"
+      : "private_message";
   const sendMessage = { from, to, text, type };
   axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", sendMessage);
   document.querySelector("#sendText").value = "Escreva aqui...";
+  getMessage();
 }
 
 function processResponse(response) {
@@ -63,13 +80,89 @@ function processResponse(response) {
         </div>
         `;
     }
+    if (messages[i].to === userNameInput) {
+      if (messages[i].type === "private_message") {
+        message.innerHTML += `
+          <div class="pink-back">
+            <span class="font-grey">(${messages[i].time})</span>&nbsp<span class="strong">${messages[i].from}</span>&nbsp
+            para &nbsp<span class="strong">${messages[i].to}</span>:&nbsp<span>${messages[i].text}</span>
+          </div>
+          `;
+      }
+    }
   }
+}
+
+function usersResponse(response) {
+  let usersOnline = response.data;
+  let userOnline = document.querySelector(".contacts");
+  if (!usersOnline.some((e) => e.name === selectedUser.name)) {
+    selectedUser.name = "Todos";
+  }
+  if (selectedUser.name === "Todos") {
+    userOnline.innerHTML = `
+    <div class="all selectedContact" onclick="selectContact(this)">
+      <div>
+        <ion-icon name="people"></ion-icon>
+        <p>Todos</p>
+      </div>
+      <div>
+        <ion-icon class="check hide" name="checkmark-sharp"></ion-icon>
+      </div>
+    </div>
+    `;
+  } else {
+    userOnline.innerHTML = `
+    <div class="all" onclick="selectContact(this)">
+      <div>
+        <ion-icon name="people"></ion-icon>
+        <p>Todos</p>
+      </div>
+      <div>
+        <ion-icon class="check hide" name="checkmark-sharp"></ion-icon>
+      </div>
+    </div>
+    `;
+  }
+
+  for (let i = 1; i < usersOnline.length; i++) {
+    if (selectedUser.name === usersOnline[i].name) {
+      userOnline.innerHTML += `
+      <div class="userOnline selectedContact" onclick="selectContact(this)">
+        <div>
+          <ion-icon name="person-circle"></ion-icon>
+          <p>${usersOnline[i].name}</p>
+        </div>
+        <div>
+          <ion-icon class="check hide" name="checkmark-sharp"></ion-icon>
+        </div>
+      </div>
+    `;
+    } else {
+      userOnline.innerHTML += `
+      <div class="userOnline" onclick="selectContact(this)">
+        <div>
+          <ion-icon name="person-circle"></ion-icon>
+          <p>${usersOnline[i].name}</p>
+        </div>
+        <div>
+          <ion-icon class="check hide" name="checkmark-sharp"></ion-icon>
+        </div>
+      </div>
+  `;
+    }
+  }
+}
+
+function scroll() {
+  document.querySelector(".msg-box > div:last-child")?.scrollIntoView();
 }
 
 function handleEnterClick() {
   userNameInput = document.querySelector("#userNameInput").value;
   userRequest(userNameInput);
   getMessage();
+  getUsers();
   defaultSetting();
 }
 function defaultSetting() {
@@ -87,11 +180,14 @@ function hideSideBar() {
 }
 
 function selectContact(el) {
-  const selectedContact = document.querySelector(".selectedContact");
+  selectedContact = document.querySelector(".selectedContact");
   if (selectedContact !== null) {
     selectedContact.classList.remove("selectedContact");
   }
-  el.classList.add("selectedContact");
+  userSelected = el.classList.add("selectedContact");
+  selectedUser.name = document.querySelector(
+    ".selectedContact > div > p"
+  ).innerHTML;
 }
 
 function selectVisibility(el) {
